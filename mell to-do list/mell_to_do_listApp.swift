de3ccfdd778afabd2abc8ffa48a74c892971c22f
BrunaMello <brunacrespomello@gmail.com>
2023-05-14 11:54:07 +0100

    Initial Commit

---
+++ mell to-do list/mell_to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  mell_to_do_listApp.swift
+//  mell to-do list
+//
+//  Created by Bruna Mello on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct mell_to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
