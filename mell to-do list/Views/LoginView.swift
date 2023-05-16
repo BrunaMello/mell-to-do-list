1f581901660f0f5c5cb9b71df07f7bb651f7424b
BrunaMello <brunacrespomello@gmail.com>
2023-05-16 13:10:11 +0100

    Lesson 3: Header View – SwiftUI To Do List
    In this video we will learn how to make a SwiftUI view generic and reusable. Specifically we will work with the header view that will be common across the Log In and Register screens. We'll leverage parameters to make this component configurable and flexible. By the end of this video, you'll have mastered proper view reusability in SwiftUI.

--- mell to-do list/Views/LoginView.swift
+++ mell to-do list/Views/LoginView.swift
@@ -18,7 +18,7 @@ struct LoginView: View {
         NavigationView {
             VStack {
                 // Header
-                HeaderView()
+                HeaderView(title: "To Do List", subtitle: "Get things done", angle: 15, background: .pink)
                 
                 // Login form
                 Form {
@@ -41,6 +41,7 @@ struct LoginView: View {
                     }
                     .padding()
                 }
+                .offset(y: -15)
                 
                 //Create Account
                 
