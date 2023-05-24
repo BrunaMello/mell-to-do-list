af7b3dcff4301710ea511895ebe3c44beeaf6c02
BrunaMello <brunacrespomello@gmail.com>
2023-05-24 13:13:30 +0100

    Lesson 14: App Icon & Launch Screen – SwiftUI To Do List
    In this video we will wrap up our SwiftUI To Do List App with a cool Launch Screen and new App Icon. We'll discuss how you can create the Launch Screen and add an App Icon asset to your Xcode Asset Catalog.

--- mell to-do list/ViewsModels/ToDoListItemViewViewModel.swift
+++ mell to-do list/ViewsModels/ToDoListItemViewViewModel.swift
@@ -4,10 +4,28 @@
 //
 //  Created by Bruna Mello on 14/05/2023.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 // View model single to do list item view(each row in items list)
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // Database
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
 }
